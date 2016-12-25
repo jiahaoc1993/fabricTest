b68f764905c5f561e9da7636a2359b0f68a66aa9
LordGoodman <201negative@sina.com>
2016-12-25 04:54:59 +0000

    A Presure Test Demo(Oringinal)

--- decentSend/presureTest.go
+++ decentSend/presureTest.go
@@ -9,21 +9,22 @@ import (
 	"time"
 )
 
-func mutilFunc(f string, n int) {
-	if f == "invoke" {
-		c := make(chan int)
-		start := time.Now().Unix()
+func mutilInvoke() {
+	n, _ := strconv.Atoi(os.Args[2])
+	c := make(chan int)
+	start := time.Now().Unix()
+	go func() {
 		for i := 0; i < n; i++ {
 			method.Invoke()
 			c <- 1
 		}
-		for s := 0; s < n; {
-			s += <-c
-		}
-		end := time.Now().Unix()
-		spent := end - start
-		fmt.Println("All Done! Spent %d Seconds\n", spent)
+	}()
+	for s := 0; s < n; {
+		s += <-c
 	}
+	end := time.Now().Unix()
+	spent := end - start
+	fmt.Printf("All Done! Spent %d Seconds\n", spent)
 }
 
 func main() {
@@ -33,8 +34,7 @@ func main() {
 	case "deploy":
 		method.Deploy()
 	case "invoke":
-		i, _ := strconv.Atoi(os.Args[2])
-		mutilFunc("invoke", i)
+		mutilInvoke()
 	case "query":
 		method.Query()
 	}
