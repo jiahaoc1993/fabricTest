f37efa85bad8efefead2c6d6c4c9fece59d3ecab
LordGoodman <201negative@sina.com>
2016-12-27 07:46:18 +0000

    a question about bytes print out

--- gopacket/test.go
+++ gopacket/test.go
@@ -4,11 +4,23 @@ import (
 	//"bytes"
 	//	"encoding/binary"
 	"fmt"
+	"net"
 )
 
-func main() {
-	b := []byte{255, 255, 255, 0}
-	for _, h := range b {
-		fmt.Printf("%x\n", h)
-	}
+type testIN struct {
+	B []byte
 }
+
+func main() {
+	var mask net.IPMask
+	//b := []byte{255, 255, 255, 0}
+	b := make([]byte, 4)
+	b[0] = 255
+	b[1] = 255
+	b[2] = 255
+	b[3] = 0
+	mask = b
+	fmt.Println(mask[0])
+}
+
+//I finally found out why address.net return bytes ffffff00 rather than 255.255.255.255 , the type of address.net is net.IPMask
