febe532a9767ff89d793fbe32fadfdc1b3bd46da
LordGoodman <201negative@sina.com>
2016-12-27 08:16:43 +0000

    finished quescion of bytes print out difference in test.go

--- gopacket/test.go
+++ gopacket/test.go
@@ -4,23 +4,34 @@ import (
 	//"bytes"
 	//	"encoding/binary"
 	"fmt"
-	"net"
+	//"net"
 )
 
 type testIN struct {
 	B []byte
 }
 
+const (
+	hexDigit = "0123456789abcdef"
+)
+
+func hexstring(b []byte) string { //all right , this is the secret, seprate one byte(8 bits) into two grop of 4 bits, each group represent a hex digit
+	s := make([]byte, 2*len(b))
+	for i, tn := range b {
+		s[2*i], s[2*i+1] = hexDigit[tn>>4], hexDigit[tn&0xf]
+	}
+	return string(s)
+}
+
 func main() {
-	var mask net.IPMask
+
 	//b := []byte{255, 255, 255, 0}
 	b := make([]byte, 4)
 	b[0] = 255
 	b[1] = 255
 	b[2] = 255
 	b[3] = 0
-	mask = b
-	fmt.Println(mask[0])
+	fmt.Println(b, hexstring(b))
 }
 
 //I finally found out why address.net return bytes ffffff00 rather than 255.255.255.255 , the type of address.net is net.IPMask
