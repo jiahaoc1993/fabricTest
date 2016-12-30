597b184a668fb743aee7001efefa75338d320ce9
LordGoodman <201negative@sina.com>
2016-12-30 13:48:11 +0000

    still working, coming soon

--- EvalNode/client.go
+++ EvalNode/client.go
@@ -1,6 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+	"golang.org/x/net/context"
+	"github.com/golang/protobuf/jsonpb"
+	pb "github.com/hyperledger/fabric/protos"
+
+
+type Transaction struct {
+	Jsonrpc *string				`json:"jsonrpc,omitempty"`
+	Method  *string				`json:"method,omitempty"`
+	Params  *pb.ChaincodeSpec   `json:"params,omitempty"`
+	ID      *rpcID
+	}
+
+type rpcID struct {
+	StringValue *string
+	IntValue    *int64
+	}
+
+func MakeATransaction() {
+	t := &Transaction{
+		"2.0",
+		"deploy",
+		Params{
+			1,
+
+		}
+	}
+
+	}
+
+
+
+func Deploy() {
+
+	}
 
 func main() {
 	fmt.Println("vim-go")
