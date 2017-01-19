f73ef459bca548fe460f8b9306c25ab2d382b231
LordGoodman <201negative@sina.com>
2017-01-19 03:41:54 +0000

    uptdate the viper test

--- EvalNode/test.go
+++ EvalNode/test.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"strings"
 	"fmt"
+	"github.com/spf13/viper"
 	//"tool/loadKey"
-	pb "github.com/hyperledger/fabric/protos"
-	"tool/rpc"
+	//pb "github.com/hyperledger/fabric/protos"
+	//"tool/rpc"
 )
 
 func main() {
-	response := rpc.Connect(&pb.Transaction{})
-	fmt.Println(response)
+	viper.SetEnvPrefix("core")
+	viper.AutomaticEnv()
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	viper.AddConfigPath("/opt/gopath/src/github.com/hyperledger/fabric/peer/")
+	viper.SetConfigName("core")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error when reading config file"))
+	}
+
+	fmt.Println(viper.GetInt("peer.gomaxprocs"))
 }
