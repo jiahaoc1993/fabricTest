208812cf8dfd667d642f636ee685085149e85ac0
LordGoodman <201negative@sina.com>
2017-02-25 08:23:25 +0000

    remove js

--- javascript/array.js
+++
@@ -1,4 +0,0 @@
-var numbers = [1,2,3,4,5,6,7]
-numbers.forEach(function(item, index, array){
-		console.log("the index of %d is %d",item,index)
-	})
--- javascript/object.js
+++
@@ -1,3 +0,0 @@
-var persion = new Object
-persion.name = "luke"
-console.log(persion.name)
