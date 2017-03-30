236b2424091a968b90294b4ad2c62ed03560166f
LordGoodman <201negative@sina.com>
2017-03-30 17:21:54 +0800

    nearly done!

---
+++ simpleHttp/front/www/js/home.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+	var user = getUrlParam('name')
+	$("#accountName").text(user)
+	query(user)
+	
+	$("#topup").click(function(){
+		$("#txType").val("topup");
+		$("#amount").attr("placeholder","100 USD");
+		$("#fee").val("0 USD");
+	});
+
+	$("#invest").click(function(){
+		$("#txType").val("invest");
+		$("#amount").attr("placeholder","100 GP Coins");
+		$("#fee").val("5% USD");
+	});
+
+	$("#cashout").click(function(){
+		$("#txType").val("cashout");
+		$("#amount").attr("placeholder","100 GP Coins");
+		$("#fee").val("5% USD");
+
+	});
+
+	$("#transfer").click(function(){
+		$(".transfer").show()
+		$("#txType").val("transfer");
+		$("#amount").attr("placeholder","100 GP Coins");
+		$("#fee").val("0 USD");	
+
+	});
+	
+	$("#middleTx").click(function(){
+		  user = getUrlParam("name")
+			func   = $("#txType").val()
+			amount = $("#amount").val()
+			args = {
+				"User" : user,
+				"Amount": amount,
+				}
+			middleTx(func, args)
+			
+			 $('.dialog').hide();
+      // $('.dialog2').hide();
+	});
+	
+	$("#transferTx").click(function(){
+			user = getUrlParam("name")
+			amount   = $("#amount2").val()
+			receiver = $("#party").val()
+			args = {
+			"From"   :   user,
+			"Amount" :   amount,
+			"To" : receiver,	
+			}
+			transferTx(args)
+			$(".dialog").hide();
+			$(".dialog2").hide();
+	});	
+})
+
+function query(user){
+		$.post("http://127.0.0.1:8000/query", {"User": user},
+			function(result){
+					if (result.state){
+					$("#GPCoin").text(result.gpcoin + " GpCoin");
+					$("#USD").text(result.usd + " USD");
+				}else{
+					alert("not user was offer")
+					}
+		}
+	);
+}
+
+function middleTx(func, args){
+	$.post("http://127.0.0.1:8000/"+func, args)
+	//alert(args.User+func+args.Amount)
+	query(args.User)
+}
+
+function transferTx(args){
+	$.post("http://127.0.0.1:8000/transfer", args)
+	//alert(args.From+args.Amount+args.Receiver)
+	query(args.From)
+}
+
+function getUrlParam(name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); 
+            var r = window.location.search.substr(1).match(reg);  
+            if (r != null) return unescape(r[2]); return null; 
+        }
---
+++ simpleHttp/front/www/www/js/home.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	var user = getUrlParam('name')
+	$("#accountName").text(user)
+		$.post("http://127.0.0.1:8000/query", {"user":"Luke"},
+			function(result, status){
+					alert(result+status);
+		}
+	);
+	
+	$("#topup").click(function(){
+		$("#txType").val("top up");
+		$("#amount").attr("placeholder","100 USD");
+		$("#fee").val("0 USD");
+	});
+
+	$("#invest").click(function(){
+		$("#txType").val("invest");
+		$("#amount").attr("placeholder","100 GP Coins");
+		$("#fee").val("5% USD");
+	});
+
+	$("#cashout").click(function(){
+		$("#txType").val("cash out");
+		$("#amount").attr("placeholder","100 GP Coins");
+		$("#fee").val("5% USD");
+	});
+
+	$("#transfer").click(function(){
+		$("#txType").val("transfer");
+		$("#amount").attr("placeholder","100 GP Coins");
+		$("#fee").val("0 USD");
+	});	
+	
+})
+
+
+ function getUrlParam(name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); 
+            var r = window.location.search.substr(1).match(reg);  
+            if (r != null) return unescape(r[2]); return null; 
+        }
