393c9996593a7960d8fb9269261babb24d3eeaa0
LordGoodman <201negative@sina.com>
2017-03-31 15:18:57 +0000

    ready to add api

--- simpleHttp/server/httpHandler.go
+++ simpleHttp/server/httpHandler.go
@@ -18,7 +18,7 @@ type response struct{
 
 type User struct{
 	GpCoin int
-	USD    int	
+	USD    int
 }
 
 var USERs = map[string]User{
@@ -44,7 +44,7 @@ func queryHandler(w http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(w, "not find any user!")
 		return
 	}
-	
+
 	res := response{
 		Status : true,
 		GpCoin : USERs[user[0]].GpCoin,
@@ -67,15 +67,14 @@ func topupHandler(w http.ResponseWriter, request *http.Request) {
 	amount, found2:= request.Form["Amount"]
 	if !(found1 && found2) {
 		fmt.Fprint(w, "not find any user!")
-		return 
+		return
 	}
 	usd, _ := strconv.Atoi(amount[0])
-	
 	var tmp =  User{
 		GpCoin: USERs[user[0]].GpCoin,
-		USD:    USERs[user[0]].USD + usd , 
+		USD:    USERs[user[0]].USD + usd ,
 	}
-	
+
 	USERs[user[0]] = tmp
 }
 
@@ -101,29 +100,27 @@ func transferHandler(w http.ResponseWriter, request *http.Request) {
 	from, found1  := request.Form["From"]
 	to, found2    := request.Form["To"]
 	amount, found3:= request.Form["Amount"]
-	
+
 	if !(found1 && found2 && found3) {
 		fmt.Fprint(w, "not find any user!")
-		return 
+		return
 	}
-	
+
 	trans, _ := strconv.Atoi(amount[0])
-	
+
 	if trans > USERs[from[0]].GpCoin{
 		fmt.Fprint(w, "You don't have enough money!")
-		return 
+		return
 		}
 	var fromTmp =  User{
 		GpCoin: USERs[from[0]].GpCoin - trans,
-		USD:    USERs[from[0]].USD , 
+		USD:    USERs[from[0]].USD ,
 	}
-	
-	
+
   var toTmp =  User{
 		GpCoin: USERs[to[0]].GpCoin + trans,
-		USD:    USERs[to[0]].USD , 
+		USD:    USERs[to[0]].USD ,
 	}
-	
 	USERs[from[0]] = fromTmp
 	USERs[to[0]] = toTmp
 
@@ -142,7 +139,7 @@ func main() {
 	//router.HandleFunc("/invest", investHandler).Method("POST")
 	//router.HandleFunc("/cashout", cashoutHandler).Method("POST")
 	router.HandleFunc("/transfer", transferHandler).Methods("POST")
-	
+
 	http.Handle("/", router)
 	http.ListenAndServe(":8000", nil)
 }
